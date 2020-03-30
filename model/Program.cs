0c64a7b76423124701c682123718318b731a3fcc
Yimar22 <yimar2001@gmail.com>
2020-03-30 12:28:38 -0500

    First commit, json read, and form

--- model/Program.cs
+++ model/Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using SODA;
+using SODA.Models;
+using SODA.Utilities;
+using Json.Net;
+using System.Net;
+using System.IO;
 
-namespace model
+namespace airQualityData
 {
     class Program
     {
+
+        public static String URL = "https://www.datos.gov.co/resource/ysq6-ri4e.json?";
+
+        private static String DATA = "";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            read(URL + "$limit=5");
+
+            var dataSet = JsonNet.Deserialize<Dictionary<string, string>[]>(DATA);
+
+           // Console.WriteLine(dataSet[0]);
+
+            Thread.Sleep(60 * (Int32)Math.Pow(10, 3));
+
+        }
+
+        private static void read(String data)
+        {
+            try
+            {
+                var url = data;
+                var client = new WebClient();
+                using (var stream = client.OpenRead(url))
+                using (var reader = new StreamReader(stream))
+                {
+                    String line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        DATA += line;
+                        line = reader.ReadLine();
+                    }
+                    reader.Close();
+                    stream.Close();
+                }
+
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
--- ui/Program.cs
+++ ui/Program.cs
@@ -16,7 +16,7 @@ namespace ui
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Busqueda());
         }
     }
 }
